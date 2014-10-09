3c071cf5927e8562d454f1faab6cf6fcc28715cc
Rok Krajnc <rok.krajnc@gmail.com>
2014-10-09 22:16:46 +0200

    fixed denise clut & ham generator so a normal registered RAM can be used.

--- rtl/minimig/denise_colortable.v
+++ rtl/minimig/denise_colortable.v
@@ -5,38 +5,43 @@
 
 module denise_colortable
 (
-	input	clk,					// 35ns pixel clock
-  input clk7_en,
-	input 	[8:1] reg_address_in,	// register adress inputs
-	input 	[11:0] data_in,			// bus data in
-	input	[5:0] select,			// colour select input
-  input a1k,              // EHB control
-	output	reg [11:0] rgb			// RGB output
+  input  wire           clk,              // 28MHz clock
+  input  wire           clk7_en,          // 7MHz clock enable
+  input  wire [  9-1:1] reg_address_in,   // register adress inputs
+  input  wire [ 12-1:0] data_in,          // bus data in
+  input  wire [  6-1:0] select,           // colour select input
+  input  wire           a1k,              // EHB control
+  output reg  [ 12-1:0] rgb               // RGB output
 );
 
-// register names and adresses		
-parameter COLORBASE = 9'h180;  		// colour table base address
+// register names and adresses
+parameter COLORBASE = 9'h180;         // colour table base address
 
 // local signals
-reg 	[11:0] colortable [31:0];	// colour table
-wire	[11:0] selcolor; 			// selected colour register output
+reg   [ 12-1:0] colortable [0:32-1];  // colour table
+reg   [ 12-1:0] selcolor;             // selected colour register output
 
-// writing of colour table from bus (implemented using dual port distributed ram)
-always @(posedge clk)
+// writing of colour table from bus (implemented using dual port ram)
+always @ (posedge clk) begin
   if (clk7_en) begin
-  	if (reg_address_in[8:6]==COLORBASE[8:6])
-  		colortable[reg_address_in[5:1]] <= data_in[11:0];
+    if (reg_address_in[8:6] == COLORBASE[8:6])
+      colortable[reg_address_in[5:1]] <= #1 data_in[11:0];
   end
+end
 
 // reading of colour table
-assign selcolor = colortable[select[4:0]];   
+always @ (posedge clk) begin
+  selcolor <= #1 colortable[select[4:0]];
+end
 
 // extra half brite mode shifter
-always @(posedge clk)
-	if (select[5] && !a1k) // half bright, shift every component 1 position to the right
-		rgb <= {1'b0,selcolor[11:9],1'b0,selcolor[7:5],1'b0,selcolor[3:1]};
-	else // normal colour select
-		rgb <= selcolor;
+always @ (*) begin
+  if (select[5] && !a1k) // half bright, shift every component 1 position to the right
+    rgb <= {1'b0,selcolor[11:9],1'b0,selcolor[7:5],1'b0,selcolor[3:1]};
+  else // normal colour select
+    rgb <= selcolor;
+end
+
 
 endmodule
 
--- rtl/minimig/denise_hamgenerator.v
+++ rtl/minimig/denise_hamgenerator.v
@@ -1,54 +1,60 @@
 // this module handles the hold and modify mode (HAM)
-// the module has its own colour pallete bank, this is to let 
-// the sprites run simultanously with a HAM playfield
+// the module has its own colour pallete bank, this is to let
+// the sprites run simultaneously with a HAM playfield
 
 
 module denise_hamgenerator
 (
-	input	clk,					// 35ns pixel clock
-  input clk7_en,
-	input 	[8:1] reg_address_in,	// register adress inputs
-	input 	[11:0] data_in,			// bus data in
-	input	[5:0] bpldata,			// bitplane data input
-	output	reg [11:0] rgb			// RGB output
+  input  wire           clk,              // 28MHz clock
+  input  wire           clk7_en,          // 7MHz clock enable
+  input  wire [  9-1:1] reg_address_in,   // register adress inputs
+  input  wire [ 12-1:0] data_in,          // bus data in
+  input  wire [  6-1:0] bpldata,          // bitplane data input
+  output reg  [ 12-1:0] rgb               // RGB output
 );
 
-//register names and adresses		
-parameter COLORBASE = 9'h180;  		// colour table base address
+// register names and adresses
+parameter COLORBASE = 9'h180;         // colour table base address
 
-//local signals
-reg 	[11:0] colortable [15:0];	// colour table
-wire	[11:0] selcolor;			// selected colour output from colour table
+// local signals
+reg   [ 12-1:0] colortable [0:16-1];  // colour table
+reg   [ 12-1:0] selcolor;             // selected colour register output
+reg   [ 12-1:0] rgb_prev;             // previous rgb value
 
-//--------------------------------------------------------------------------------------
-
-//writing of HAM colour table from bus (implemented using dual port distributed ram)
-/*always @(posedge clk28m)*/
-always @(posedge clk)
+// writing of HAM colour table from bus (implemented using dual port ram)
+always @ (posedge clk) begin
   if (clk7_en) begin
-  	if (reg_address_in[8:5]==COLORBASE[8:5])
-  		colortable[reg_address_in[4:1]] <= data_in[11:0];
+    if (reg_address_in[8:5] == COLORBASE[8:5])
+      colortable[reg_address_in[4:1]] <= #1 data_in[11:0];
   end
-
-//reading of colour table
-assign selcolor = colortable[bpldata[3:0]];   
-
-//--------------------------------------------------------------------------------------
-
-//HAM instruction decoder/processor
-always @(posedge clk)
-begin
-	case (bpldata[5:4])
-		2'b00://load rgb output with colour from table	
-			rgb <= selcolor;
-		2'b01://hold green and red, modify blue
-			rgb  <= {rgb[11:4],bpldata[3:0]};	
-		2'b10://hold green and blue, modify red
-			rgb <= {bpldata[3:0],rgb[7:0]};
-		2'b11://hold blue and red, modify green
-			rgb <= {rgb[11:8],bpldata[3:0],rgb[3:0]};
-	endcase
 end
+
+// reading of colour table
+always @ (posedge clk) begin
+  selcolor <= #1 colortable[bpldata[3:0]];
+end
+
+// register previous rgb value
+always @ (posedge clk) begin
+  rgb_prev <= #1 rgb;
+end
+
+// HAM instruction decoder/processor
+always @ (*) begin
+  case (bpldata[5:4])
+    2'b00: // load rgb output with colour from table
+      rgb <= selcolor;
+    2'b01: // hold green and red, modify blue
+      rgb  <= {rgb_prev[11:4],bpldata[3:0]};
+    2'b10: // hold green and blue, modify red
+      rgb <= {bpldata[3:0],rgb_prev[7:0]};
+    2'b11: // hold blue and red, modify green
+      rgb <= {rgb_prev[11:8],bpldata[3:0],rgb_prev[3:0]};
+    default:
+      rgb <= selcolor;
+  endcase
+end
+
 
 endmodule
 
